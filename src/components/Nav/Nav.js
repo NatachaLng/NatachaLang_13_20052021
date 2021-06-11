60a5ad4e2d11dbc0307553fe5245b75689868efd
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-06-11 16:59:12 +0100

    update NAV

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,28 +1,65 @@
-import React from "react";
-import "./Nav.css";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Link } from "react-router-dom";
 import logo from "../../img/argentBankLogo.png"
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { faUserCircle, faSignInAlt, faSignOutAlt } from '@fortawesome/fontawesome-free-solid'
 
-class Nav extends React.Component{
-    render(){
-        return(
+import { logout } from "../../actions/actions";
+import "./Nav.css";
+
+
+
+class Nav extends Component {
+    render() {
+        return (
             <nav className="main-nav">
-            <a className="main-nav-logo" href="/">
-                <img
-                    className="main-nav-logo-image"
-                    src={logo}
-                    alt="Argent Bank Logo"
-                />
-                <h1 className="sr-only">Argent Bank</h1>
-            </a>
-            <div>
-                <a className="main-nav-item" href="./login">
-                    <i className="fa fa-user-circle"></i>
-                    Sign In
-                </a>
-            </div>
-        </nav>
-        )
+                <Link className="main-nav-logo" to="/">
+                    <img
+                        className="main-nav-logo-image"
+                        src={logo}
+                        alt="Logo Argent Bank "
+                    />
+                    <h1 className="sr-only">Argent Bank</h1>
+                </Link>
+                {!this.props.user.logStatus ? (
+                    <div>
+                        <Link className="main-nav-item" to="/login">
+                            <FontAwesomeIcon icon={faUserCircle} />
+                            Sign In
+                        </Link>
+                    </div>
+                ) : (
+                    <div className="main-nav-logout">
+                        <FontAwesomeIcon icon={faSignInAlt} />
+                        <span className="main-nav-name">{this.props.user.firstName}</span>
+                        <Link
+                            className="main-nav-item"
+                            to="/"
+                            onClick={() => this.props.logout()}
+                        >
+                            <FontAwesomeIcon icon={faSignOutAlt} />
+                            Sign out
+                        </Link>
+                    </div>
+                )}
+            </nav>
+        );
     }
 }
 
-export default Nav
+const mapStateToProps = (state) => ({
+    user: state.user,
+});
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators(
+        {
+            logout,
+        },
+        dispatch
+    );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);

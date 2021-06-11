0047123727487419d02a86768e938cd6d19d5c39
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-06-11 21:02:22 +0100

    update swagger

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { Link } from "react-router-dom";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import {Link} from "react-router-dom";
 import logo from "../../img/argentBankLogo.png"
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { faUserCircle, faSignInAlt, faSignOutAlt } from '@fortawesome/fontawesome-free-solid'
+import signIn from "../../img/sign-in-alt-solid.svg"
+import signOut from "../../img/sign-out-alt-solid.svg"
+import user from "../../img/user-circle-solid.svg"
 
-import { logout } from "../../actions/actions";
+
+import {logout} from "../../actions/actions";
 import "./Nav.css";
-
 
 
 class Nav extends Component {
@@ -26,22 +27,26 @@ class Nav extends Component {
                 {!this.props.user.logStatus ? (
                     <div>
                         <Link className="main-nav-item" to="/login">
-                            <FontAwesomeIcon icon={faUserCircle} />
+                            <img src={signIn} width={"20px"} className={"nav-img"}/>
                             Sign In
                         </Link>
                     </div>
                 ) : (
                     <div className="main-nav-logout">
-                        <FontAwesomeIcon icon={faSignInAlt} />
-                        <span className="main-nav-name">{this.props.user.firstName}</span>
-                        <Link
-                            className="main-nav-item"
-                            to="/"
-                            onClick={() => this.props.logout()}
-                        >
-                            <FontAwesomeIcon icon={faSignOutAlt} />
-                            Sign out
-                        </Link>
+                        <div className='main-nav-item'>
+                            <img src={user} width={"20px"} className={"nav-img"}/>
+                            <span className="main-nav-name">{this.props.user.firstName}</span>
+                        </div>
+                        <div className='main-nav-item'>
+                            <Link
+                                className="main-nav-item"
+                                to="/"
+                                onClick={() => this.props.logout()}
+                            >
+                                <img src={signOut} width={"20px"} className={"nav-img"}/>
+                                Sign out
+                            </Link>
+                        </div>
                     </div>
                 )}
             </nav>
